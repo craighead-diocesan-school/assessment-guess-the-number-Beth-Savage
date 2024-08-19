48c8077a665016c06d861b31e6f2405ec2b0c47c
Beth-Savage <savagebe49@craighead.school.nz>
2024-08-20 10:30:23 +1200

    Boundary testing
    Tested the limits of the computers choices 1-20 are options and work.
    Added a trim function
    Tested with
    13 |  4   | 7              |
    All work

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 function guessTheNumber() {
   let name = prompt("What is your name?")
   alert("Hello " + name + "! We are going to play a game. I will think of a number from 1 to 20 and you will have to guess it. Take as many turns as you need.")
-  let chosenNumber = Math.floor(Math.random() * 19)
+  let chosenNumber = Math.floor(Math.random() * 20)
   chosenNumber = chosenNumber + 1
   let usersGuess = 0
-  let attemps = 0
+  let attempts = 0
+  alert(chosenNumber)
   while (usersGuess != chosenNumber) {
-    attemps = attemps + 1
+    attempts = attempts + 1
     usersGuess = prompt("What number do you think?")
+    usersGuess = usersGuess.trim()
     usersGuess = Number(usersGuess)
-    if (usersGuess != chosenNumber) {
-      alert("Incorrect, try again.")
+    if (usersGuess < chosenNumber) {
+      alert("Incorrect, too small.")
+    } else if (usersGuess > chosenNumber) {
+      alert("Incorrect, too big.")
     } else if (usersGuess == chosenNumber) {
       alert("Correct, well done!")
     }
