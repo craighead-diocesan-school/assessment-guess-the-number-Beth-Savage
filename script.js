2078681eb8357afb44c226bd4310e14ba3530511
Beth-Savage <savagebe49@craighead.school.nz>
2024-08-22 10:44:33 +1200

    Improved comments
    Improved the comments with more specific examples and referencing which line of code it is referencing.

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 let scoreboard = []
 function guessTheNumber() {
-  //Asks the user thier name then welcomes them and indroduces the game
+  //name = prompt asks the user their name and then the next line welcomes them and indroduces the game
   let name = prompt("What is your name?")
   alert("Hello " + name + "! We are going to play a game. I will think of a number from 1 to 20 and you will have to guess it. Take as many turns as you need.")
-  //The computer chooses a random number for the user to guess. As the range of *20 is 0 - 19 it adds one on to make it a valid guess.
+  //Math.random makes the computer choose a random number for the user to guess. As the range of *20 is 0 - 19 it then adds one on to make it a valid number inside the range.
   let chosenNumber = Math.floor(Math.random() * 20)
   chosenNumber = chosenNumber + 1
-  //creates varibles and their values before they are changed in later code
+  //creates varibles and their values before they are used and sometimes changed in later code
   let usersGuess = 0
   let attempts = 0
   let lowerLimit = 1
   let upperLimit = 20
-  //Repeats the game until the user guesses the number correctly and adds one to the round counter every repeat
+  //The while loop will repeat the game until the user guesses the number correctly and adds one to the round counter every repeat
   while (usersGuess != chosenNumber) {
     attempts = attempts + 1
-    //Asks the user their guess then takes away spaces and turns it into a number
+    //usersGuess = prompt asks the user their guess then uses .trim() to take away spaces and usersGuess = Number to turn it into a number
     usersGuess = prompt("What number do you think?")
     usersGuess = usersGuess.trim()
     usersGuess = Number(usersGuess)
@@ -31,10 +31,10 @@ function guessTheNumber() {
       alert("Something went wrong please try again")
     }
   }
-  //Adds onto the scoreboard the users inputted name and the number of guesses it took for them to guess the number on a new line
+  //scoreboard.push adds onto the scoreboard, the users inputted name and the number of guesses it took for them to guess the number. \n means that when outputted it will be on a new line
   scoreboard.push("\n" + name + ": " + attempts)
 }
-//Shows the scoreboard
+//The showScoreboard() function shows the scoreboard with an explaination of what the numbers mean when the button connected to it is pressed
 function showScoreboard() {
   alert("The Scoreboard: the number beside someone's name is the number of guesses it took them to guess the number." + scoreboard)
 }
