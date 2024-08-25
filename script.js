f0613735ecf79ddab7aed974d3a7b3c34e0b91f0
Beth-Savage <savagebe49@craighead.school.nz>
2024-08-26 09:30:32 +1200

    Made the code more flexible
    Moved the variables to the start and used the upper and lower limit ones more in the code to make it easier to be changed.

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let scoreboard = []
 function guessTheNumber() {
-  //name = prompt asks the user their name and then the next line welcomes them and indroduces the game
-  let name = prompt("What is your name?")
-  alert("Hello " + name + "! We are going to play a game. I will think of a number from 1 to 20 and you will have to guess it. Take as many turns as you need.")
-  //Math.random makes the computer choose a random number for the user to guess. As the range of *20 is 0 - 19 it then adds one on to make it a valid number inside the range.
-  let chosenNumber = Math.floor(Math.random() * 20)
-  chosenNumber = chosenNumber + 1
-  //creates varibles and their values before they are used and sometimes changed in later code
+  //creates varibles and their values before they are used and some are changed in later code
   let usersGuess = 0
   let attempts = 0
   let lowerLimit = 1
   let upperLimit = 20
+  //name = prompt asks the user their name and then the next line welcomes them and indroduces the game
+  let usersName = prompt("What is your name?")
+  alert("Hello " + usersName + "! We are going to play a game. I will think of a number from " + lowerLimit + " to " + upperLimit + " and you will have to guess it. Take as many turns as you need.")
+  //Math.random makes the computer choose a random number for the user to guess. As the range of *upperLimit (in this scenario *20) is 0 - 19 it then adds one on to make it a valid number inside the range.
+  let chosenNumber = Math.floor(Math.random() * upperLimit)
+  chosenNumber = chosenNumber + 1
   //The while loop will repeat the game until the user guesses the number correctly and adds one to the round counter every repeat
   while (usersGuess != chosenNumber) {
     attempts = attempts + 1
@@ -32,7 +32,7 @@ function guessTheNumber() {
     }
   }
   //scoreboard.push adds onto the scoreboard, the users inputted name and the number of guesses it took for them to guess the number. \n means that when outputted it will be on a new line
-  scoreboard.push("\n" + name + ": " + attempts)
+  scoreboard.push("\n" + usersName + ": " + attempts)
 }
 //The showScoreboard() function shows the scoreboard with an explaination of what the numbers mean when the button connected to it is pressed
 function showScoreboard() {
